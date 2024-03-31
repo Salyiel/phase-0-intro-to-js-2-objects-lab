6459db113cc4f47262e433c537715eb9dd5450a0
Stacy <salyiela@icloud.com>
2024-03-31 16:39:44 +0300

    Completed lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const employee = {
+    name: 'sam',
+    streetAddress: '11 Tumbleweed',
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, name, Gregory) {
+    const newEmployee = {...employee};
+
+    newEmployee[name] = Gregory;
+
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, toad) {
+    employee[name] = toad;
+
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+
+    const newEmployee = {...employee};
+
+    delete newEmployee[key];
+
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    delete employee[key];
+
+    return employee;
+}
